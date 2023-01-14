b06cf509c461dab0b63eceb96828e3efb614de9d
Daniil Samoilov <daniilsvin@gmail.com>
2023-01-14 10:14:07 +0600

    downgrade dart version, fix dep for dev

--- dgis_maps_flutter/lib/dgis_maps_flutter.dart
+++ dgis_maps_flutter/lib/dgis_maps_flutter.dart
@@ -1,7 +1,24 @@
 library dgis_maps_flutter;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class DGisMap extends StatefulWidget {
+  const DGisMap({Key? key}) : super(key: key);
+
+  @override
+  State<DGisMap> createState() => _DGisMapState();
+}
+
+class _DGisMapState extends State<DGisMap> {
+  @override
+  Widget build(BuildContext context) {
+    return const AndroidView(
+      viewType: 'dgis_maps_flutter',
+      // onPlatformViewCreated: onPlatformViewCreated,
+      // gestureRecognizers: widgetConfiguration.gestureRecognizers,
+      // creationParams: creationParams,
+      creationParamsCodec: StandardMessageCodec(),
+    );
+  }
 }
